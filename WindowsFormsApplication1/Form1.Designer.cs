ff9e16d0423eedac1e56f95526c469b84aedce37
mh0817 <my.name.m.h.0817@gmail.com>
2016-10-27 23:02:03 +0900

    Add files via upload

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,330 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Calculator
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_No7 = new System.Windows.Forms.Button();
+            this.button_No6 = new System.Windows.Forms.Button();
+            this.button_No5 = new System.Windows.Forms.Button();
+            this.button_No3 = new System.Windows.Forms.Button();
+            this.button_No2 = new System.Windows.Forms.Button();
+            this.button_No1 = new System.Windows.Forms.Button();
+            this.button_No4 = new System.Windows.Forms.Button();
+            this.button_No9 = new System.Windows.Forms.Button();
+            this.button_No8 = new System.Windows.Forms.Button();
+            this.button_times = new System.Windows.Forms.Button();
+            this.button_sub = new System.Windows.Forms.Button();
+            this.button_add = new System.Windows.Forms.Button();
+            this.button_devides = new System.Windows.Forms.Button();
+            this.button_Point = new System.Windows.Forms.Button();
+            this.button_No0 = new System.Windows.Forms.Button();
+            this.Display_NumberInput = new System.Windows.Forms.TextBox();
+            this.button_Clear = new System.Windows.Forms.Button();
+            this.button_Equal = new System.Windows.Forms.Button();
+            this.Display_Calc_Number = new System.Windows.Forms.TextBox();
+            this.Disp_Calc_Symbol = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button_No7
+            // 
+            this.button_No7.Font = new System.Drawing.Font("MS UI Gothic", 25F);
+            this.button_No7.Location = new System.Drawing.Point(17, 150);
+            this.button_No7.Name = "button_No7";
+            this.button_No7.Size = new System.Drawing.Size(50, 50);
+            this.button_No7.TabIndex = 0;
+            this.button_No7.Text = "7";
+            this.button_No7.UseVisualStyleBackColor = true;
+            this.button_No7.Click += new System.EventHandler(this.button_Number_Click);
+            // 
+            // button_No6
+            // 
+            this.button_No6.Font = new System.Drawing.Font("MS UI Gothic", 25F);
+            this.button_No6.Location = new System.Drawing.Point(163, 207);
+            this.button_No6.Name = "button_No6";
+            this.button_No6.Size = new System.Drawing.Size(50, 50);
+            this.button_No6.TabIndex = 1;
+            this.button_No6.Text = "6";
+            this.button_No6.UseVisualStyleBackColor = true;
+            this.button_No6.Click += new System.EventHandler(this.button_Number_Click);
+            // 
+            // button_No5
+            // 
+            this.button_No5.Font = new System.Drawing.Font("MS UI Gothic", 25F);
+            this.button_No5.Location = new System.Drawing.Point(87, 207);
+            this.button_No5.Name = "button_No5";
+            this.button_No5.Size = new System.Drawing.Size(50, 50);
+            this.button_No5.TabIndex = 2;
+            this.button_No5.Text = "5";
+            this.button_No5.UseVisualStyleBackColor = true;
+            this.button_No5.Click += new System.EventHandler(this.button_Number_Click);
+            // 
+            // button_No3
+            // 
+            this.button_No3.Font = new System.Drawing.Font("MS UI Gothic", 25F);
+            this.button_No3.Location = new System.Drawing.Point(163, 263);
+            this.button_No3.Name = "button_No3";
+            this.button_No3.Size = new System.Drawing.Size(50, 50);
+            this.button_No3.TabIndex = 3;
+            this.button_No3.Text = "3";
+            this.button_No3.UseVisualStyleBackColor = true;
+            this.button_No3.Click += new System.EventHandler(this.button_Number_Click);
+            // 
+            // button_No2
+            // 
+            this.button_No2.Font = new System.Drawing.Font("MS UI Gothic", 25F);
+            this.button_No2.Location = new System.Drawing.Point(87, 263);
+            this.button_No2.Name = "button_No2";
+            this.button_No2.Size = new System.Drawing.Size(50, 50);
+            this.button_No2.TabIndex = 4;
+            this.button_No2.Text = "2";
+            this.button_No2.UseVisualStyleBackColor = true;
+            this.button_No2.Click += new System.EventHandler(this.button_Number_Click);
+            // 
+            // button_No1
+            // 
+            this.button_No1.Font = new System.Drawing.Font("MS UI Gothic", 25F);
+            this.button_No1.Location = new System.Drawing.Point(17, 263);
+            this.button_No1.Name = "button_No1";
+            this.button_No1.Size = new System.Drawing.Size(50, 50);
+            this.button_No1.TabIndex = 5;
+            this.button_No1.Text = "1";
+            this.button_No1.UseVisualStyleBackColor = true;
+            this.button_No1.Click += new System.EventHandler(this.button_Number_Click);
+            // 
+            // button_No4
+            // 
+            this.button_No4.Font = new System.Drawing.Font("MS UI Gothic", 25F);
+            this.button_No4.Location = new System.Drawing.Point(17, 207);
+            this.button_No4.Name = "button_No4";
+            this.button_No4.Size = new System.Drawing.Size(50, 50);
+            this.button_No4.TabIndex = 6;
+            this.button_No4.Text = "4";
+            this.button_No4.UseVisualStyleBackColor = true;
+            this.button_No4.Click += new System.EventHandler(this.button_Number_Click);
+            // 
+            // button_No9
+            // 
+            this.button_No9.Font = new System.Drawing.Font("MS UI Gothic", 25F);
+            this.button_No9.Location = new System.Drawing.Point(163, 150);
+            this.button_No9.Name = "button_No9";
+            this.button_No9.Size = new System.Drawing.Size(50, 50);
+            this.button_No9.TabIndex = 7;
+            this.button_No9.Text = "9";
+            this.button_No9.UseVisualStyleBackColor = true;
+            this.button_No9.Click += new System.EventHandler(this.button_Number_Click);
+            // 
+            // button_No8
+            // 
+            this.button_No8.Font = new System.Drawing.Font("MS UI Gothic", 25F);
+            this.button_No8.Location = new System.Drawing.Point(87, 150);
+            this.button_No8.Name = "button_No8";
+            this.button_No8.Size = new System.Drawing.Size(50, 50);
+            this.button_No8.TabIndex = 8;
+            this.button_No8.Text = "8";
+            this.button_No8.UseVisualStyleBackColor = true;
+            this.button_No8.Click += new System.EventHandler(this.button_Number_Click);
+            // 
+            // button_times
+            // 
+            this.button_times.Font = new System.Drawing.Font("MS UI Gothic", 25F);
+            this.button_times.Location = new System.Drawing.Point(240, 263);
+            this.button_times.Name = "button_times";
+            this.button_times.Size = new System.Drawing.Size(50, 50);
+            this.button_times.TabIndex = 9;
+            this.button_times.Tag = "*";
+            this.button_times.Text = "×";
+            this.button_times.UseVisualStyleBackColor = true;
+            this.button_times.Click += new System.EventHandler(this.button_Calc_Click);
+            // 
+            // button_sub
+            // 
+            this.button_sub.Font = new System.Drawing.Font("MS UI Gothic", 25F);
+            this.button_sub.Location = new System.Drawing.Point(240, 207);
+            this.button_sub.Name = "button_sub";
+            this.button_sub.Size = new System.Drawing.Size(50, 50);
+            this.button_sub.TabIndex = 10;
+            this.button_sub.Tag = "-";
+            this.button_sub.Text = "-";
+            this.button_sub.UseVisualStyleBackColor = true;
+            this.button_sub.Click += new System.EventHandler(this.button_Calc_Click);
+            // 
+            // button_add
+            // 
+            this.button_add.Font = new System.Drawing.Font("MS UI Gothic", 25F);
+            this.button_add.Location = new System.Drawing.Point(240, 150);
+            this.button_add.Name = "button_add";
+            this.button_add.Size = new System.Drawing.Size(50, 50);
+            this.button_add.TabIndex = 11;
+            this.button_add.Tag = "+";
+            this.button_add.Text = "+";
+            this.button_add.UseVisualStyleBackColor = true;
+            this.button_add.Click += new System.EventHandler(this.button_Calc_Click);
+            // 
+            // button_devides
+            // 
+            this.button_devides.Font = new System.Drawing.Font("MS UI Gothic", 25F);
+            this.button_devides.Location = new System.Drawing.Point(240, 319);
+            this.button_devides.Name = "button_devides";
+            this.button_devides.Size = new System.Drawing.Size(50, 50);
+            this.button_devides.TabIndex = 12;
+            this.button_devides.Tag = "/";
+            this.button_devides.Text = "÷";
+            this.button_devides.UseVisualStyleBackColor = true;
+            this.button_devides.Click += new System.EventHandler(this.button_Calc_Click);
+            // 
+            // button_Point
+            // 
+            this.button_Point.Font = new System.Drawing.Font("MS UI Gothic", 25F);
+            this.button_Point.Location = new System.Drawing.Point(163, 319);
+            this.button_Point.Name = "button_Point";
+            this.button_Point.Size = new System.Drawing.Size(50, 50);
+            this.button_Point.TabIndex = 13;
+            this.button_Point.Text = "未";
+            this.button_Point.UseVisualStyleBackColor = true;
+            // 
+            // button_No0
+            // 
+            this.button_No0.Font = new System.Drawing.Font("MS UI Gothic", 25F);
+            this.button_No0.Location = new System.Drawing.Point(17, 319);
+            this.button_No0.Name = "button_No0";
+            this.button_No0.Size = new System.Drawing.Size(120, 50);
+            this.button_No0.TabIndex = 14;
+            this.button_No0.Text = "0";
+            this.button_No0.UseVisualStyleBackColor = true;
+            this.button_No0.Click += new System.EventHandler(this.button_Number_Click);
+            // 
+            // Display_NumberInput
+            // 
+            this.Display_NumberInput.Font = new System.Drawing.Font("MS UI Gothic", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.Display_NumberInput.Location = new System.Drawing.Point(17, 73);
+            this.Display_NumberInput.Name = "Display_NumberInput";
+            this.Display_NumberInput.Size = new System.Drawing.Size(273, 47);
+            this.Display_NumberInput.TabIndex = 15;
+            this.Display_NumberInput.Text = "0";
+            this.Display_NumberInput.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // button_Clear
+            // 
+            this.button_Clear.Font = new System.Drawing.Font("MS UI Gothic", 25F);
+            this.button_Clear.Location = new System.Drawing.Point(310, 150);
+            this.button_Clear.Name = "button_Clear";
+            this.button_Clear.Size = new System.Drawing.Size(50, 50);
+            this.button_Clear.TabIndex = 16;
+            this.button_Clear.Text = "C";
+            this.button_Clear.UseVisualStyleBackColor = true;
+            this.button_Clear.Click += new System.EventHandler(this.button_Clear_Click);
+            // 
+            // button_Equal
+            // 
+            this.button_Equal.Font = new System.Drawing.Font("MS UI Gothic", 25F);
+            this.button_Equal.Location = new System.Drawing.Point(310, 207);
+            this.button_Equal.Name = "button_Equal";
+            this.button_Equal.Size = new System.Drawing.Size(50, 50);
+            this.button_Equal.TabIndex = 17;
+            this.button_Equal.Text = "=";
+            this.button_Equal.UseVisualStyleBackColor = true;
+            this.button_Equal.Click += new System.EventHandler(this.button_Equal_Click);
+            // 
+            // Display_Calc_Number
+            // 
+            this.Display_Calc_Number.Font = new System.Drawing.Font("MS UI Gothic", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.Display_Calc_Number.Location = new System.Drawing.Point(17, 12);
+            this.Display_Calc_Number.Name = "Display_Calc_Number";
+            this.Display_Calc_Number.ReadOnly = true;
+            this.Display_Calc_Number.Size = new System.Drawing.Size(273, 47);
+            this.Display_Calc_Number.TabIndex = 18;
+            this.Display_Calc_Number.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // Disp_Calc_Symbol
+            // 
+            this.Disp_Calc_Symbol.Font = new System.Drawing.Font("MS UI Gothic", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.Disp_Calc_Symbol.Location = new System.Drawing.Point(310, 12);
+            this.Disp_Calc_Symbol.Name = "Disp_Calc_Symbol";
+            this.Disp_Calc_Symbol.ReadOnly = true;
+            this.Disp_Calc_Symbol.Size = new System.Drawing.Size(50, 47);
+            this.Disp_Calc_Symbol.TabIndex = 19;
+            this.Disp_Calc_Symbol.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.Disp_Calc_Symbol.Click += new System.EventHandler(this.button_Number_Click);
+            // 
+            // Calculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 411);
+            this.Controls.Add(this.Disp_Calc_Symbol);
+            this.Controls.Add(this.Display_Calc_Number);
+            this.Controls.Add(this.button_Equal);
+            this.Controls.Add(this.button_Clear);
+            this.Controls.Add(this.Display_NumberInput);
+            this.Controls.Add(this.button_No0);
+            this.Controls.Add(this.button_Point);
+            this.Controls.Add(this.button_devides);
+            this.Controls.Add(this.button_add);
+            this.Controls.Add(this.button_sub);
+            this.Controls.Add(this.button_times);
+            this.Controls.Add(this.button_No8);
+            this.Controls.Add(this.button_No9);
+            this.Controls.Add(this.button_No4);
+            this.Controls.Add(this.button_No1);
+            this.Controls.Add(this.button_No2);
+            this.Controls.Add(this.button_No3);
+            this.Controls.Add(this.button_No5);
+            this.Controls.Add(this.button_No6);
+            this.Controls.Add(this.button_No7);
+            this.Name = "Calculator";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_No7;
+        private System.Windows.Forms.Button button_No6;
+        private System.Windows.Forms.Button button_No5;
+        private System.Windows.Forms.Button button_No3;
+        private System.Windows.Forms.Button button_No2;
+        private System.Windows.Forms.Button button_No1;
+        private System.Windows.Forms.Button button_No4;
+        private System.Windows.Forms.Button button_No9;
+        private System.Windows.Forms.Button button_No8;
+        private System.Windows.Forms.Button button_times;
+        private System.Windows.Forms.Button button_sub;
+        private System.Windows.Forms.Button button_add;
+        private System.Windows.Forms.Button button_devides;
+        private System.Windows.Forms.Button button_Point;
+        private System.Windows.Forms.Button button_No0;
+        private System.Windows.Forms.TextBox Display_NumberInput;
+        private System.Windows.Forms.Button button_Clear;
+        private System.Windows.Forms.Button button_Equal;
+        private System.Windows.Forms.TextBox Display_Calc_Number;
+        private System.Windows.Forms.TextBox Disp_Calc_Symbol;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Calculator : Form
+    {
+        public Calculator()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Number_Click(object sender, EventArgs e)
+        {
+            if (Display_NumberInput.Text == "0")
+            {
+                Display_NumberInput.Text = ((Button)sender).Text;
+            }
+            else
+            {
+                Display_NumberInput.Text += ((Button)sender).Text;
+            }
+            
+        }
+
+        private void button_Clear_Click(object sender, EventArgs e)
+        {
+            Display_NumberInput.Text = Convert.ToString("0");
+            Display_Calc_Number.ResetText();
+            Disp_Calc_Symbol.ResetText();
+        }
+
+        private void button_Calc_Click(object sender, EventArgs e)
+        {
+            if (Disp_Calc_Symbol.Text == "")
+            {
+                Display_Calc_Number.Text = Display_NumberInput.Text;
+                Disp_Calc_Symbol.Text = ((Button)sender).Text;
+            }
+            else
+            {
+                if(Display_Calc_Number.Text == "")
+                {
+                    Display_Calc_Number.Text = Display_NumberInput.Text;
+                }
+                else
+                {
+                    int Equal_data = 0;
+                    int Input_data = Convert.ToInt32(Display_Calc_Number.Text);
+                    int Calc_data = Convert.ToInt32(Display_NumberInput.Text);
+
+                    if (Disp_Calc_Symbol.Text == "+")
+                    {
+                        Equal_data = Input_data + Calc_data;
+                    }
+                    else if (Disp_Calc_Symbol.Text == "-")
+                    {
+                        Equal_data = Input_data - Calc_data;
+                    }
+                    else if (Disp_Calc_Symbol.Text == "×")
+                    {
+                        Equal_data = Input_data * Calc_data;
+                    }
+                    else if (Disp_Calc_Symbol.Text == "÷")
+                    {
+                        if (Calc_data == 0)
+                        {
+                            Equal_data = Input_data;
+                        }
+                        else
+                        {
+                            Equal_data = (Input_data / Calc_data);
+                        }
+                    }
+
+                    Display_Calc_Number.Text = Convert.ToString(Equal_data);
+                }
+
+                Disp_Calc_Symbol.Text = ((Button)sender).Text;
+            }
+            
+            Display_NumberInput.Text = Convert.ToString("0");
+        }
+
+        private void button_Equal_Click(object sender, EventArgs e)
+        {
+            int Input_data = 0;
+            if (Display_Calc_Number.Text != "")
+            {
+                Input_data = Convert.ToInt32(Display_Calc_Number.Text);
+            }
+            int Calc_data = Convert.ToInt32(Display_NumberInput.Text);
+
+            int Equal_data = 0;
+            if(Disp_Calc_Symbol.Text != "")
+            {
+                if (Disp_Calc_Symbol.Text == "+")
+                {
+                    Equal_data = Input_data + Calc_data;
+                }
+                else if (Disp_Calc_Symbol.Text == "-")
+                {
+                    Equal_data = Input_data - Calc_data;
+                }
+                else if (Disp_Calc_Symbol.Text == "×")
+                {
+                    Equal_data = Input_data * Calc_data;
+                }
+                else if (Disp_Calc_Symbol.Text == "÷")
+                {
+                    if (Calc_data == 0)
+                    {
+                        Equal_data = Input_data;
+                    }
+                    else
+                    {
+                        Equal_data = (Input_data / Calc_data);
+                    }
+                }
+                Display_NumberInput.Text = Convert.ToString(Equal_data);
+                Display_Calc_Number.ResetText();
+                Disp_Calc_Symbol.ResetText();
+            }
+
+
+
+        }
+    }
+}
